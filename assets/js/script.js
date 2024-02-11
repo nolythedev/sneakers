cdf44803837ddf592f802d808851a09e2287d7a9
nolythedev <nolychigwedere@hotmail.co.uk>
2024-02-11 01:17:15 +0000

    Responsive styling added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let imageMain = document.querySelector('#main-image');
 let productImage = document.querySelectorAll('.product');
 
 let currentImage = document.querySelector('#main-image').dataset.image;
+
 let qtyCount = 0;
 
 // console.log(currentImage
@@ -14,20 +15,26 @@ console.log('current image:' + currentImage);
 
 // grid section TODO:
 // on click of image1, image2 or image3, mainImage should be swapped to clicked image and main image should be swapped to the clicked image
+
+// iterate through product images 
 for (let i = 0; i < productImage.length; i++) {
     const product = productImage[i];
-    console.log(product.dataset.image);
-    
+    // console.log(product.dataset.image);
+ 
+// click event for product images
 product.addEventListener('click', (event) => {
     event.preventDefault();
-    // event.target.style.backgroundImage = `url(../assets/images/${currentImage}.jpg)`;
-    
+  
+    // set background of main image to clicked product image
     imageMain.style.backgroundImage = `url(../assets/images/${event.target.dataset.image}.jpg)`;
+    // set the main image data attr to the attr of the clicked product image
     imageMain.dataset.image = event.target.dataset.image;
+    // set the current image var to be the clicked product's data attr
     event.target.dataset.image = currentImage;
+    // set the background image of the clicked image to the previous current image data attr
     event.target.style.backgroundImage = `url(../assets/images/${currentImage}.jpg)`;
+    // update the current image to the new main image's data attr 
     currentImage = imageMain.dataset.image;
-
 
 });
 
